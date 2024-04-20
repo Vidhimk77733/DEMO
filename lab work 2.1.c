6de7b14d33f87c535890ee33838eeede206e7f52
JaydeepThakar <163496807+Jaydeepthakar@users.noreply.github.com>
2024-04-20 09:12:01 +0530

    lab work 2

---
+++ lab work 2.1.c
@@ -0,0 +1,18 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a=12, b=6, c;
+    
+    c=a+b;
+    printf("addition=%d /n",c);
+    c=a-b;
+    printf("subtraction=%d /n",c);
+    c=a*b ;
+    printf("multiplication=%d /n",c);
+    c=a/b;
+    printf("division=5d/n",c);
+    c=a%b;
+    printf("modulo=%d /n",c);
+    return 0;
+}
